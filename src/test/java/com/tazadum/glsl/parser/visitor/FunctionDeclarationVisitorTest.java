5b970ddabb0d35f9e887ef974a61c277fd43e057
Erik Byström <erik.bystrom@gmail.com>
2016-09-17 11:20:07 +0200

    More extensive test of Funtion parsing

--- src/test/java/com/tazadum/glsl/parser/visitor/FunctionDeclarationVisitorTest.java
+++ src/test/java/com/tazadum/glsl/parser/visitor/FunctionDeclarationVisitorTest.java
@@ -2,8 +2,10 @@ package com.tazadum.glsl.parser.visitor;
 
 import com.tazadum.glsl.ast.Node;
 import com.tazadum.glsl.ast.function.FunctionDeclarationNode;
+import com.tazadum.glsl.ast.variable.ParameterDeclarationNode;
 import com.tazadum.glsl.language.BuiltInType;
 import com.tazadum.glsl.language.GLSLParser;
+import com.tazadum.glsl.language.TypeQualifier;
 import com.tazadum.glsl.parser.ParserContext;
 import com.tazadum.glsl.parser.TestUtils;
 import org.antlr.v4.runtime.CommonTokenStream;
@@ -28,7 +30,56 @@ public class FunctionDeclarationVisitorTest {
         assertEquals("main", node.getIdentifier().original());
         assertEquals(BuiltInType.VOID, node.getReturnType().getType());
         assertEquals(0, node.getChildCount());
+    }
 
+    @Test
+    public void test_type_void() {
+        FunctionDeclarationNode node;
+
+        node = parse("vec3 main()");
+        assertEquals("main", node.getIdentifier().original());
+        assertEquals(BuiltInType.VEC3, node.getReturnType().getType());
+        assertEquals(0, node.getChildCount());
+
+        node = parse("float main(void)");
+        assertEquals("main", node.getIdentifier().original());
+        assertEquals(BuiltInType.FLOAT, node.getReturnType().getType());
+        assertEquals(0, node.getChildCount());
+    }
+
+    @Test
+    public void test_void_type() {
+        FunctionDeclarationNode node;
+        ParameterDeclarationNode parameter;
+
+        node = parse("void main(float a)");
+        assertEquals("main", node.getIdentifier().original());
+        assertEquals(BuiltInType.VOID, node.getReturnType().getType());
+        assertEquals(1, node.getChildCount());
+
+        parameter = node.getChildren(ParameterDeclarationNode.class).iterator().next();
+        assertEquals(BuiltInType.FLOAT, parameter.getFullySpecifiedType().getType());
+        assertEquals("a", parameter.getIdentifier().original());
+
+        node = parse("void main(out float a)");
+        assertEquals("main", node.getIdentifier().original());
+        assertEquals(BuiltInType.VOID, node.getReturnType().getType());
+        assertEquals(1, node.getChildCount());
+
+        parameter = node.getChildren(ParameterDeclarationNode.class).iterator().next();
+        assertEquals(BuiltInType.FLOAT, parameter.getFullySpecifiedType().getType());
+        assertEquals(TypeQualifier.OUT, parameter.getFullySpecifiedType().getQualifier());
+        assertEquals("a", parameter.getIdentifier().original());
+    }
+
+    @Test
+    public void test_void_types() {
+        FunctionDeclarationNode node;
+
+        node = parse("void main(float a, float b)");
+        assertEquals("main", node.getIdentifier().original());
+        assertEquals(BuiltInType.VOID, node.getReturnType().getType());
+        assertEquals(2, node.getChildCount());
     }
 
     private FunctionDeclarationNode parse(String code) {
